22580a23e86416109ab1cf6bf12f23528f3846de
Aarchi Jain <93213027+aarchijain1@users.noreply.github.com>
2022-03-02 23:37:32 +0530

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,45 +1,61 @@
-document.getElementsByClassName("tabs")[0].addEventListener("click",function(){
-  document.getElementsByClassName("content")[0].style.display="block";
-  document.getElementsByClassName("content")[1].style.display="none";
-  document.getElementsByClassName("content")[2].style.display="none";
-  document.getElementsByClassName("content")[3].style.display="none";
-  document.getElementsByClassName("tabs")[0].classList.add("active");
-  document.getElementsByClassName("tabs")[1].classList.remove("active");
-  document.getElementsByClassName("tabs")[2].classList.remove("active");
-  document.getElementsByClassName("tabs")[3].classList.remove("active");
+function tab(evt, no) {
+  var i, content, tabs;
 
-});
-document.getElementsByClassName("tabs")[1].addEventListener("click",function(){
-  document.getElementsByClassName("content")[0].style.display="none";
-  document.getElementsByClassName("content")[1].style.display="block";
-  document.getElementsByClassName("content")[2].style.display="none";
-  document.getElementsByClassName("content")[3].style.display="none";
-  document.getElementsByClassName("tabs")[0].classList.remove("active");
-  document.getElementsByClassName("tabs")[1].classList.add("active");
-  document.getElementsByClassName("tabs")[2].classList.remove("active");
-  document.getElementsByClassName("tabs")[3].classList.remove("active");
+  content = document.getElementsByClassName("content");
+  for (i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
+  }
 
-});
-document.getElementsByClaassName("tabs")[2].addEventListener("click",function(){
-  document.getElementsByClassName("content")[0].style.display="none";
-  document.getElementsByClassName("content")[1].style.display="none";
-  document.getElementsByClassName("content")[3].style.display="none";
-  document.getElementsByClassName("content")[2].style.display="block";
-  document.getElementsByClassName("tabs")[0].classList.remove("active");
-  document.getElementsByClassName("tabs")[1].classList.remove("active");
-  document.getElementsByClassName("tabs")[3].classList.remove("active");
-  document.getElementsByClassName("tabs")[2].classList.add("active");
+  tabs = document.getElementsByClassName("tabs");
+  for (i = 0; i < tabs.length; i++) {
+    tabs[i].className = tabs[i].className.replace(" active", "");
+  }
+  document.getElementById(no).style.display = "block";
+  evt.currentTarget.className += " active";
+}
 
-});
+// document.getElementsByClassName("tabs")[0].addEventListener("click",function(){
+//   document.getElementsByClassName("content")[0].style.display="block";
+//   document.getElementsByClassName("content")[1].style.display="none";
+//   document.getElementsByClassName("content")[2].style.display="none";
+//   document.getElementsByClassName("content")[3].style.display="none";
+//   document.getElementsByClassName("tabs")[0].classList.add("active");
+//   document.getElementsByClassName("tabs")[1].classList.remove("active");
+//   document.getElementsByClassName("tabs")[2].classList.remove("active");
+//   document.getElementsByClassName("tabs")[3].classList.remove("active");
 
-document.getElementsByClassName("tabs")[3].addEventListener("click",function(){
-  document.getElementsByClassName("content")[0].style.display="none";
-  document.getElementsByClassName("content")[1].style.display="none";
-  document.getElementsByClassName("content")[2].style.display="none";
-  document.getElementsByClassName("content")[3].style.display="block";
-  document.getElementsByClassName("tabs")[0].classList.remove("active");
-  document.getElementsByClassName("tabs")[1].classList.remove("active");
-  document.getElementsByClassName("tabs")[2].classList.remove("active");
-  document.getElementsByClassName("tabs")[3].classList.add("active");
+// });
+// document.getElementsByClassName("tabs")[1].addEventListener("click",function(){
+//   document.getElementsByClassName("content")[0].style.display="none";
+//   document.getElementsByClassName("content")[1].style.display="block";
+//   document.getElementsByClassName("content")[2].style.display="none";
+//   document.getElementsByClassName("content")[3].style.display="none";
+//   document.getElementsByClassName("tabs")[0].classList.remove("active");
+//   document.getElementsByClassName("tabs")[1].classList.add("active");
+//   document.getElementsByClassName("tabs")[2].classList.remove("active");
+//   document.getElementsByClassName("tabs")[3].classList.remove("active");
 
-});
+// });
+// document.getElementsByClaassName("tabs")[2].addEventListener("click",function(){
+//   document.getElementsByClassName("content")[0].style.display="none";
+//   document.getElementsByClassName("content")[1].style.display="none";
+//   document.getElementsByClassName("content")[3].style.display="none";
+//   document.getElementsByClassName("content")[2].style.display="block";
+//   document.getElementsByClassName("tabs")[0].classList.remove("active");
+//   document.getElementsByClassName("tabs")[1].classList.remove("active");
+//   document.getElementsByClassName("tabs")[3].classList.remove("active");
+//   document.getElementsByClassName("tabs")[2].classList.add("active");
+
+// });
+
+// document.getElementsByClassName("tabs")[3].addEventListener("click",function(){
+//   document.getElementsByClassName("content")[0].style.display="none";
+//   document.getElementsByClassName("content")[1].style.display="none";
+//   document.getElementsByClassName("content")[2].style.display="none";
+//   document.getElementsByClassName("content")[3].style.display="block";
+//   document.getElementsByClassName("tabs")[0].classList.remove("active");
+//   document.getElementsByClassName("tabs")[1].classList.remove("active");
+//   document.getElementsByClassName("tabs")[2].classList.remove("active");
+//   document.getElementsByClassName("tabs")[3].classList.add("active");
+
+// });
